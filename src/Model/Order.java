d98539783ba3d1dea36e14fe066f31cb262a6843
Moozey <anavoinea821@yahoo.com>
2023-04-08 21:47:15 +0300

    Added Order model

---
+++ src/Model/Order.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private int id;
+    private Date data;
+
+    public Order(int id, Date data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    private List<Product> listOfProducts;
+}
