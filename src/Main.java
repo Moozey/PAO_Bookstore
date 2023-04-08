d8ee2e4ad5d1f4c9f5cb6273e2dcb7a7ee787019
Moozey <anavoinea821@yahoo.com>
2023-04-08 21:41:51 +0300

    Added iml and Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Service.Functionality;
+
+public class Main {
+    public static void main(String[] args) {
+        Functionality service = new Functionality();
+        service.MyMethod();
+    }
+}
