817422cf5f007d7179346384eb8513a68f719503
Moozey <anavoinea821@yahoo.com>
2023-04-08 21:46:42 +0300

    Added DVD model

---
+++ src/Model/DVD.java
@@ -0,0 +1,31 @@
+package Model;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class DVD extends Product {
+    private Band band;
+    private Type type;
+
+    public DVD(int id, String title, String description, double price, Band band, Type type) {
+        super(id, title, description, price);
+        this.band = band;
+        this.type = type;
+    }
+
+    public Band getBand() {
+        return band;
+    }
+
+    public void setBand(Band band) {
+        this.band = band;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
