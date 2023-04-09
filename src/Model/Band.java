1994e78d6a3da7ef04b4d82548d61bf8a7897db8
Moozey <anavoinea821@yahoo.com>
2023-04-09 16:51:56 +0300

    Modified Band's constructor and setter and tested the changes

--- src/Model/Band.java
+++ src/Model/Band.java
@@ -1,23 +1,30 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Band {
     private String bandName;
+    private List<Person> listOfPersons;
 
     public Band(String bandName) {
         this.bandName = bandName;
+        this.listOfPersons = new ArrayList<Person>();
     }
 
     public String getBandName() {
         return bandName;
     }
-
     public void setBandName(String bandName) {
         this.bandName = bandName;
     }
+    public List<Person> getListOfPersons() {
+        return listOfPersons;
+    }
 
-    private List<Person> listOfPersons;
+    public void addPersonToBand(Person personToBeAdded) {
+        this.listOfPersons.add(personToBeAdded);
+    }
 
     @Override
     public String toString() {
--- src/Service/Functionality.java
+++ src/Service/Functionality.java
@@ -8,11 +8,23 @@ public class Functionality {
         Product p2 = new Product( "creion", "tool to draw with", 1.05);
         System.out.println(p1);
         System.out.println(p2);
+
         Person per1 = new Person("Ana", "Voinea");
         Author author1 = new Author("razvan", "ilie", "romania");
         System.out.println(per1);
         System.out.println(author1);
         System.out.println(author1.getId());
+
+        Band band1 = new Band("ROOOOOOOOCK");
+        System.out.println(band1.getListOfPersons());
+
+        Person per2 = new Person("harry", "potter");
+        band1.addPersonToBand(per2);
+        System.out.println(band1.getListOfPersons());
+
+        band1.addPersonToBand(author1);
+        System.out.println(band1.getListOfPersons());
+        System.out.println(band1);
     }
 
 
